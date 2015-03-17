c1aada15e4f0b4ea5e029ac63ca21b100e151e82
Minhtri Tran <mtran@eg.poly.edu>
2015-03-17 18:40:41 -0400

    Added in bootstrap and basic template

---
+++ barter/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from barter import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.home, name='home'),
+    url(r'^user/$', views.user, name='user')
+)
--- barter/views.py
+++ barter/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'barter/index.html', {})
+def user(request):
+    return render(request, 'barter/user.html', {"foo": "bar"})
