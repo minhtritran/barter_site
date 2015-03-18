a634b217f9a944155b83417a1c10387dbcf304bf
at2706 <at2706@nyu.edu>
2015-03-17 20:00:08 -0400

    some models

--- barter/models.py
+++ barter/models.py
@@ -1,3 +1,31 @@
+import datetime
 from django.db import models
+from django.contrib.auth import models
+from django.utils import timezone
+
 
 # Create your models here.
+class User(models.User):
+    DOB = models.DateTimeField('date published')
+    feedbacks = models.ManyToManyField(Feedback)
+
+    def get_feedback(self):
+        return Feedback.objects.filter(sender=self.id)
+
+
+class Feedback(models.Model):
+    sender = models.ForeignKey(User)
+    receiver = models.ForeignKey(User)
+    rating = models.IntegerField(default=0)
+    pubDate = models.DateTimeField('date published')
+    message = models.CharField(max_length=200)
+
+    def edit(self,m):
+        message = m
+        return self
+
+
+class Favor(models.Model):
+    author = models.ForeignKey(User)
+    categories = models.ArrayField(models.CharField(max_length=16), size=6)
+
