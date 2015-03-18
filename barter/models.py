b7a5a539b35454f0608fe90a08479cb0191ec73d
Minhtri Tran <mtran@eg.poly.edu>
2015-03-18 19:59:12 -0400

    These files causes a merge conflict. I need to commit them in order to start solving the merge conflict

--- barter/models.py
+++ barter/models.py
@@ -1,20 +1,96 @@
 import datetime
 import django.contrib.auth.models
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 from django.utils import timezone
 
 
 # Create your models here.
-class User(django.contrib.auth.models.User):
-    DOB = models.DateField('Date of Birth')
 
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'email']
+class UserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
 
-    def get_feedback(self):
-        return Feedback.objects.filter(sender=self.id)
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
 
-    def get_favors_completed(self):
-        return Favor.objects.filter(status='complete', completed_by=self.id)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(email,
+            password=password,
+            date_of_birth=date_of_birth
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(max_length=30, default=None, blank=True, null=True)
+    last_name = models.CharField(max_length=30, default=None, blank=True, null=True)
+    date_of_birth = models.DateField(default=None, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    #REQUIRED_FIELDS = ['date_of_birth']
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        #return self.email
+        if self.first_name is None or self.last_name is None:
+            return self.email
+        else:
+            return self.first_name + " " + self.last_name
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        if self.first_name is None:
+            return self.email
+        else:
+            return self.first_name
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
 
 
 class Feedback(models.Model):
--- barter/views.py
+++ barter/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from barter.forms import UserCreateForm
+from barter.admin import UserCreationForm
 
 # Create your views here.
 @login_required
@@ -11,5 +12,10 @@ def user(request):
     return render(request, 'barter/user.html', {"foo": "bar"})
 
 def register(request):
-    form = UserCreateForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/")
+    form = UserCreationForm()
     return render(request, 'registration/register.html', {"form": form})
