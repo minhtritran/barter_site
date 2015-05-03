ae361d6b5ce3679a1234cd3d42cea87fa632e064
Minhtri Tran <mtran@eg.poly.edu>
2015-05-02 21:48:03 -0400

    Made search indexing real-time

--- barter_site/settings.py
+++ barter_site/settings.py
@@ -134,6 +134,8 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
