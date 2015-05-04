d9164c5753654a511b7b4472ba642d95a3de3987
at2706 <at2706@nyu.edu>
2015-05-03 23:25:34 -0400

    Debug Fix again

--- barter_site/settings.py
+++ barter_site/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '&jl4+*(nv+ja+u&8$lo9qrjjbyg52c*ctdd&ux(ue*xsko7swn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['45.55.228.164']
 
